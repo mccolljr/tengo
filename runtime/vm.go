b1246cf8a4ab838c0f947536654e38b87f3b0a80
Jacob McCollum <jacob.r.mccollum@gmail.com>
2019-04-28 09:57:25 -0400

    cleanup

--- runtime/vm.go
+++ runtime/vm.go
@@ -1,6 +1,7 @@
 package runtime
 
 import (
+	"bytes"
 	"fmt"
 	"sync/atomic"
 	"unsafe"
@@ -1069,6 +1070,51 @@ func (v *VM) processInst() (die bool) {
 	}
 
 	return false
+}
+
+func (v *VM) callObject(what objects.Object, args []objects.Object) (objects.Object, error) {
+
+	numArgs := len(args)
+
+	fn := &objects.CompiledFunction{
+		Instructions: bytes.Join([][]byte{
+			compiler.MakeInstruction(compiler.OpCall, numArgs),
+			compiler.MakeInstruction(compiler.OpBreak),
+		}, []byte{}),
+		SourceMap: map[int]source.Pos{},
+	}
+
+	// create a new empty call frame
+	v.curFrame.ip = v.ip // store current ip before call
+	v.curFrame = &v.frames[v.framesIndex]
+	v.curFrame.fn = fn
+	v.curFrame.freeVars = nil
+	v.curFrame.basePointer = v.sp
+	v.curInsts = fn.Instructions
+	v.ip = -1
+	v.framesIndex++
+
+	v.stack[v.sp] = what
+	v.sp++
+	for _, arg := range args {
+		v.stack[v.sp] = arg
+		v.sp++
+	}
+
+	v.run()
+	if v.err != nil {
+		return nil, v.err
+	}
+
+	retVal := v.stack[v.sp-1]
+
+	v.framesIndex--
+	v.curFrame = &v.frames[v.framesIndex-1]
+	v.curInsts = v.curFrame.fn.Instructions
+	v.ip = v.curFrame.ip
+	v.sp = v.frames[v.framesIndex].basePointer
+
+	return retVal, nil
 }
 
 // IsStackEmpty tests if the stack is empty or not.
--- runtime/vm_compiled_function.go
+++ runtime/vm_compiled_function.go
@@ -1,9 +1,6 @@
 package runtime
 
 import (
-	"bytes"
-
-	"github.com/d5/tengo/compiler"
 	"github.com/d5/tengo/compiler/source"
 	"github.com/d5/tengo/objects"
 )
@@ -15,49 +12,4 @@ type compiledFunction struct {
 	VarArgs       bool
 	SourceMap     map[int]source.Pos
 	vmCall        func(objects.Object, []objects.Object) (objects.Object, error)
-}
-
-func (v *VM) callObject(what objects.Object, args []objects.Object) (objects.Object, error) {
-
-	numArgs := len(args)
-
-	fn := &objects.CompiledFunction{
-		Instructions: bytes.Join([][]byte{
-			compiler.MakeInstruction(compiler.OpCall, numArgs),
-			compiler.MakeInstruction(compiler.OpBreak),
-		}, []byte{}),
-		SourceMap: map[int]source.Pos{},
-	}
-
-	// update call frame
-	v.curFrame.ip = v.ip // store current ip before call
-	v.curFrame = &v.frames[v.framesIndex]
-	v.curFrame.fn = fn
-	v.curFrame.freeVars = nil
-	v.curFrame.basePointer = v.sp
-	v.curInsts = fn.Instructions
-	v.ip = -1
-	v.framesIndex++
-
-	v.stack[v.sp] = what
-	v.sp++
-	for _, arg := range args {
-		v.stack[v.sp] = arg
-		v.sp++
-	}
-
-	v.run()
-	if v.err != nil {
-		return nil, v.err
-	}
-
-	retVal := v.stack[v.sp-1]
-
-	v.framesIndex--
-	v.curFrame = &v.frames[v.framesIndex-1]
-	v.curInsts = v.curFrame.fn.Instructions
-	v.ip = v.curFrame.ip
-	v.sp = v.frames[v.framesIndex].basePointer
-
-	return retVal, nil
 }
